ecd2e5a7b8bbbb080e74ec0b9b8c704d8393cb93
Chris Pettitt <cpettitt@gmail.com>
2015-10-04 14:39:47 -0700

    Initial work on RenderSystem

---
+++ app/js/RenderSystem.js
@@ -0,0 +1,23 @@
+import THREE from "three";
+
+class RenderSystem {
+  constructor() {
+    this._renderer = new THREE.WebGLRenderer();
+    this._renderer.setPixelRatio(window.devicePixelRatio || 1);
+    document.body.appendChild(this._renderer.domElement);
+
+    this.scene = new THREE.Scene();
+    this._camera = new THREE.PerspectiveCamera(45, null, 1, 1000);
+
+    window.addEventListener("resize", () => this._onResize());
+    this._onResize();
+  }
+
+  _onResize() {
+    this._renderer.setSize(window.innerWidth, window.innerHeight);
+    this._camera.aspect = window.innerWidth / window.innerHeight;
+    this._camera.updateProjectionMatrix();
+  }
+}
+
+export default RenderSystem;
--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,8 @@
+import RenderSystem from "./RenderSystem";
+
 require("../css/main.css");
+
+const renderSystem = new RenderSystem(document.body);
+
+
 console.log("Bundle loaded!");
